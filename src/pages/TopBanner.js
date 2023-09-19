9a0b551f4b00f65185b8d7f636570c62a8067a2e
anthonymolinari <molinaria@sonoma.edu>
2023-09-19 15:06:26 -0700

    updated banner

--- src/pages/TopBanner.js
+++ src/pages/TopBanner.js
@@ -24,7 +24,7 @@ const TopBanner = () => {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 13, fontWeight: 'bold' }}>
-                        WORD-GUESS
+                        WORDLE
                     </Typography>
                     <IconButton
                         size="medium"
